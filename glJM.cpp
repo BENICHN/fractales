6569806c0178bfe6162f2b63fa9d96e15c50f987
BENICHN <nathan.benichou31280@gmail.com>
2022-05-22 15:05:48 +0200

    mandelbrot

---
+++ glJM.cpp
@@ -0,0 +1,598 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtx/norm.hpp>
+#include "imgui.h"
+#include "imgui_impl_glut.h"
+#include "imgui_impl_opengl3.h"
+
+using namespace std;
+using namespace glm;
+
+int magneti = -1;
+const float zf = 1.15;
+const int r2 = 36;
+
+const int Nmax = 1;
+GLfloat magnets[2 * Nmax] = {
+    0, 0,
+};
+
+GLuint program;
+GLuint vbo;
+GLuint magnetsLocation;
+GLuint zoomLocation;
+GLuint offsetLocation;
+GLuint fbo;
+GLuint tex;
+
+GLuint nLocation;
+GLint fs_n = 1000;
+GLuint pLocation;
+GLint fs_p = 2;
+
+GLuint useJuliaLocation;
+GLboolean fs_useJulia = false;
+GLuint useLinesLocation;
+GLboolean fs_useLines = false;
+GLuint usePointsLocation;
+GLboolean fs_usePoints = true;
+
+bool isFractalValid;
+ImVec2 fractalSize;
+ImVec2 screenPositionAbsolute;
+bool isMouseCapturedByFractal;
+bool isFractalHovered;
+int mouseX;
+int mouseY;
+
+// util functions
+
+bool readFile(const char *pFileName, string &outFile)
+{
+    ifstream f(pFileName);
+
+    bool ret = false;
+
+    if (f.is_open())
+    {
+        string line;
+        while (getline(f, line))
+        {
+            outFile.append(line);
+            outFile.append("\n");
+        }
+
+        f.close();
+
+        ret = true;
+    }
+    else
+    {
+        cout << "error while reading file " << pFileName;
+    }
+
+    return ret;
+}
+void writeBMP(char *name, int w, int h, char *img)
+{
+    int x;
+    int y;
+    int c;
+    int r;
+    int g;
+    int b;
+    FILE *f;
+    int filesize = 54 + 3 * w * h;
+
+    unsigned char bmpfileheader[14] = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
+    unsigned char bmpinfoheader[40] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0};
+    unsigned char bmppad[3] = {0, 0, 0};
+
+    bmpfileheader[2] = (unsigned char)(filesize);
+    bmpfileheader[3] = (unsigned char)(filesize >> 8);
+    bmpfileheader[4] = (unsigned char)(filesize >> 16);
+    bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+    bmpinfoheader[4] = (unsigned char)(w);
+    bmpinfoheader[5] = (unsigned char)(w >> 8);
+    bmpinfoheader[6] = (unsigned char)(w >> 16);
+    bmpinfoheader[7] = (unsigned char)(w >> 24);
+    bmpinfoheader[8] = (unsigned char)(h);
+    bmpinfoheader[9] = (unsigned char)(h >> 8);
+    bmpinfoheader[10] = (unsigned char)(h >> 16);
+    bmpinfoheader[11] = (unsigned char)(h >> 24);
+
+    f = fopen(name, "wb");
+    fwrite(bmpfileheader, 1, 14, f);
+    fwrite(bmpinfoheader, 1, 40, f);
+    for (int i = 0; i < h; i++)
+    {
+        fwrite(img + (w * (h - i - 1) * 3), 3, w, f);
+        fwrite(bmppad, 1, (4 - (w * 3) % 4) % 4, f);
+    }
+
+    free(img);
+    fclose(f);
+}
+float screenToMath(float v, float zoom, float offset) { return zoom * (v + offset); }
+float mathToScreen(float v, float zoom, float offset)
+{
+    v /= zoom;
+    return (v - offset);
+}
+
+// control functions
+
+void passiveMove(int x, int y)
+{
+    ImGui_ImplGLUT_MotionFunc(x, y);
+
+    mouseX = x;
+    mouseY = y;
+}
+void move(int x, int y)
+{
+    ImGui_ImplGLUT_MotionFunc(x, y);
+
+    if (isMouseCapturedByFractal)
+    {
+        int dx = x - mouseX;
+        int dy = y - mouseY;
+
+        if (magneti == -1)
+        {
+            GLfloat offset[2];
+            glGetUniformfv(program, offsetLocation, offset);
+            glUniform2f(offsetLocation, offset[0] - dx, offset[1] + dy);
+        }
+        else
+        {
+            GLfloat zoom;
+            glGetUniformfv(program, zoomLocation, &zoom);
+            magnets[2 * magneti] += zoom * dx;
+            magnets[2 * magneti + 1] -= zoom * dy;
+            glUniform2fv(magnetsLocation, Nmax, magnets);
+        }
+
+        isFractalValid = false;
+    }
+
+    mouseX = x;
+    mouseY = y;
+}
+void mouse(int button, int state, int x, int y)
+{
+    ImGuiIO &io = ImGui::GetIO();
+    io.AddMouseButtonEvent(button, state == GLUT_DOWN);
+    // io.AddMouseWheelEvent(0.0, state == 3 ? -io.MouseWheel : state == 4 ? io.MouseWheel
+    //                                                                     : 0.0);
+    {
+        x -= screenPositionAbsolute.x;
+        y -= screenPositionAbsolute.y;
+        y = fractalSize.y - y;
+
+        GLfloat zoom;
+        GLfloat offset[2];
+
+        float newZoom;
+        float newOffset[2];
+
+        switch (button)
+        {
+        case 0:
+            switch (state)
+            {
+            case GLUT_DOWN:
+                for (int i = 0; i < Nmax; i++)
+                {
+                    glGetUniformfv(program, offsetLocation, offset);
+                    glGetUniformfv(program, zoomLocation, &zoom);
+                    vec2 m = {mathToScreen(magnets[2 * i], zoom, offset[0]), mathToScreen(magnets[2 * i + 1], zoom, offset[1])};
+                    if (length2(m - vec2(x, y)) <= r2)
+                    {
+                        magneti = i;
+                    }
+                }
+                break;
+            case GLUT_UP:
+                magneti = -1;
+                break;
+            default:
+                break;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+    if (isFractalHovered)
+    {
+        GLfloat zoom;
+        GLfloat offset[2];
+
+        float newZoom;
+        float newOffset[2];
+
+        switch (button)
+        {
+        case 3:
+            glGetUniformfv(program, offsetLocation, offset);
+            glGetUniformfv(program, zoomLocation, &zoom);
+
+            newZoom = zoom / zf;
+            newOffset[0] = (zoom / newZoom) * (x + offset[0]) - x;
+            newOffset[1] = (zoom / newZoom) * (y + offset[1]) - y;
+
+            glUniform1f(zoomLocation, newZoom);
+            glUniform2f(offsetLocation, newOffset[0], newOffset[1]);
+
+            isFractalValid = false;
+            break;
+        case 4:
+            glGetUniformfv(program, offsetLocation, offset);
+            glGetUniformfv(program, zoomLocation, &zoom);
+
+            newZoom = zoom * zf;
+            newOffset[0] = (zoom / newZoom) * (x + offset[0]) - x;
+            newOffset[1] = (zoom / newZoom) * (y + offset[1]) - y;
+
+            glUniform1f(zoomLocation, newZoom);
+            glUniform2f(offsetLocation, newOffset[0], newOffset[1]);
+
+            isFractalValid = false;
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+void keyboard(unsigned char key, int x, int y)
+{
+    ImGui_ImplGLUT_KeyboardFunc(key, x, y);
+
+    switch (key)
+    {
+    case 'z':
+        break;
+    case 'f':
+        glutFullScreenToggle();
+        break;
+    default:
+        break;
+    }
+}
+
+void createFB(int w, int h)
+{
+    glDeleteFramebuffers(1, &fbo);
+    glDeleteTextures(1, &tex);
+
+    // creating and binding fb
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    // creating and binding tex
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    // specifying texture
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    // setting parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // unbinding texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // attaching tex to fb
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex, 0);
+
+    // checking errors
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+    // unbinding fb
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void updateVariables()
+{
+    glUniform1i(useJuliaLocation, fs_useJulia);
+    glUniform1i(useLinesLocation, fs_useLines);
+    glUniform1i(usePointsLocation, fs_usePoints);
+    glUniform1i(nLocation, fs_n);
+    glUniform1i(pLocation, fs_p);
+}
+
+void renderFractal()
+{
+    if (isFractalValid)
+        return;
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_QUADS, 0, 4);
+    glDisableVertexAttribArray(0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    isFractalValid = true;
+}
+
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+void my_display_code()
+{
+    ImGui::Begin("Fractale");
+    {
+        ImGui::BeginChild("Fractale Render");
+        ImVec2 size = ImGui::GetWindowSize();
+        if (fractalSize.x != size.x || fractalSize.y != size.y)
+        {
+            isFractalValid = false;
+            fractalSize = size;
+        }
+        if (!isFractalValid)
+            createFB(fractalSize.x, fractalSize.y);
+
+        screenPositionAbsolute = ImGui::GetItemRectMin();
+
+        isMouseCapturedByFractal = ImGui::IsItemActive();
+        isFractalHovered = ImGui::IsWindowHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem);
+
+        updateVariables();
+        renderFractal();
+
+        ImGui::Image((ImTextureID)tex, fractalSize, ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::EndChild();
+    }
+    ImGui::End();
+
+    ImGui::Begin("Paramètres");
+    {
+        if (ImGui::InputInt("p", &fs_p, 1, 2))
+            isFractalValid = false;
+        if (ImGui::DragInt("n", &fs_n, 10.0f, 0, 100000, "%.3f", ImGuiSliderFlags_Logarithmic))
+            isFractalValid = false;
+        if (ImGui::Checkbox("Julia", (bool *)&fs_useJulia))
+            isFractalValid = false;
+        if (ImGui::Checkbox("Linges", (bool *)&fs_useLines))
+            isFractalValid = false;
+        if (ImGui::Checkbox("Points", (bool *)&fs_usePoints))
+            isFractalValid = false;
+    }
+    ImGui::End();
+}
+
+void display()
+{
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+
+    my_display_code();
+
+    // Rendering
+    ImGui::Render();
+    ImGuiIO &io = ImGui::GetIO();
+    glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    // glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+void createVertexBuffer()
+{
+    vec3 vertices[4] = {
+        {-1.0f, -1.0f, 0.0f},
+        {1.0f, -1.0f, 0.0f},
+        {1.0f, 1.0f, 0.0f},
+        {-1.0f, 1.0f, 0.0f}};
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+void addShader(GLuint program, const char *pShaderText, GLenum shaderType)
+{
+    GLuint sho = glCreateShader(shaderType);
+    assert(sho != 0);
+
+    const GLchar *p[1];
+    p[0] = pShaderText;
+
+    GLint lengths[1];
+    lengths[0] = (GLint)strlen(pShaderText);
+
+    glShaderSource(sho, 1, p, lengths);
+
+    glCompileShader(sho);
+
+    GLint success;
+    glGetShaderiv(sho, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(sho, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", shaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(program, sho);
+}
+
+const char *pVSFileName = "shader.vs";
+const char *pFSFileName = "shaderJM.fs";
+void compileShaders()
+{
+    program = glCreateProgram();
+    assert(program != 0);
+
+    string vs, fs;
+
+    if (!readFile(pVSFileName, vs))
+    {
+        exit(1);
+    };
+    addShader(program, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!readFile(pFSFileName, fs))
+    {
+        exit(1);
+    };
+    addShader(program, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = {0};
+
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &Success);
+    if (Success == 0)
+    {
+        glGetProgramInfoLog(program, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(program);
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &Success);
+    if (!Success)
+    {
+        glGetProgramInfoLog(program, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(program);
+
+    // linking to uniform variables
+
+    magnetsLocation = glGetUniformLocation(program, "magnets");
+    if (magnetsLocation == -1)
+    {
+        printf("Error getting uniform location of 'magnets'\n");
+        exit(1);
+    }
+    glUniform2fv(magnetsLocation, Nmax, magnets);
+
+    zoomLocation = glGetUniformLocation(program, "zoom");
+    if (zoomLocation == -1)
+    {
+        printf("Error getting uniform location of 'zoom'\n");
+        exit(1);
+    }
+    glUniform1f(zoomLocation, 0.005);
+
+    offsetLocation = glGetUniformLocation(program, "offset");
+    if (offsetLocation == -1)
+    {
+        printf("Error getting uniform location of 'offset'\n");
+        exit(1);
+    }
+    glUniform2f(offsetLocation, -glutGet(GLUT_WINDOW_WIDTH) / 2, -glutGet(GLUT_WINDOW_HEIGHT) / 2);
+
+    pLocation = glGetUniformLocation(program, "p");
+    if (pLocation == -1)
+    {
+        printf("Error getting uniform location of 'p'\n");
+        exit(1);
+    }
+
+    nLocation = glGetUniformLocation(program, "n");
+    if (nLocation == -1)
+    {
+        printf("Error getting uniform location of 'n'\n");
+        exit(1);
+    }
+
+    useJuliaLocation = glGetUniformLocation(program, "useJulia");
+    if (useJuliaLocation == -1)
+    {
+        printf("Error getting uniform location of 'useJulia'\n");
+        exit(1);
+    }
+
+    useLinesLocation = glGetUniformLocation(program, "useLines");
+    if (useLinesLocation == -1)
+    {
+        printf("Error getting uniform location of 'useLines'\n");
+        exit(1);
+    }
+
+    usePointsLocation = glGetUniformLocation(program, "usePoints");
+    if (usePointsLocation == -1)
+    {
+        printf("Error getting uniform location of 'usePoints'\n");
+        exit(1);
+    }
+
+    updateVariables();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    int width = 1920;
+    int height = 1080;
+    glutInitWindowSize(width, height);
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("Le Newton");
+    printf("window id: %d\n", win);
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK)
+    {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigWindowsMoveFromTitleBarOnly = true;
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer backends
+    // FIXME: Consider reworking this example to install our own GLUT funcs + forward calls ImGui_ImplGLUT_XXX ones, instead of using ImGui_ImplGLUT_InstallFuncs().
+    ImGui_ImplGLUT_Init();
+    ImGui_ImplGLUT_InstallFuncs();
+    ImGui_ImplOpenGL3_Init();
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    createVertexBuffer();
+
+    compileShaders();
+
+    glutDisplayFunc(display);
+    glutPassiveMotionFunc(passiveMove);
+    glutMotionFunc(move);
+    glutMouseFunc(mouse);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGLUT_Shutdown();
+    ImGui::DestroyContext();
+
+    return 0;
+}
---
+++ shaderJM.fs
@@ -0,0 +1,198 @@
+#version 460 core
+
+in vec4 gl_FragCoord;
+out vec4 fragColor;
+
+precision highp float;
+
+// paramètres
+const int Nmax = 1;
+const int N = Nmax;
+const int r2 = 36;
+// const vec4 colors[] = vec4[](vec4(0.172,0.243,0.313,0),vec4(0.086,0.627,0.521,0),vec4(0.752,0.223,0.168,0),vec4(0.557,0.267,0.678,0));
+const vec4 colors[] = vec4[](
+    vec4(0.898,0.584,0,0),
+    vec4(0.517,0,0.196,0),
+    vec4(0,0.149,0.258,0),
+    vec4(0.898,0.854,0.854,0),
+    vec4(0.023,0.654,0.490,0),
+    vec4(0.580,0.482,0.827,0),
+    vec4(0.278,0.294,0.141,0),
+    vec4(0.898,0.454,0.737,0),
+    vec4(0.007,0.015,0.058,0)
+);
+
+uniform int n;
+uniform int p;
+
+uniform vec2 magnets[Nmax];
+uniform float zoom;
+uniform vec2 offset;
+
+uniform bool useJulia;
+uniform bool useLines;
+uniform bool usePoints;
+
+float sq(float x) { return x * x; }
+float nsq(vec2 v) { return dot(v,v); }
+
+float cabs(vec2 z)
+{
+	return length(z);
+}
+
+vec2 cconj(vec2 a)
+{
+    return vec2(a.x, -a.y);
+}
+
+vec2 cmul(vec2 a, vec2 b)
+{
+    return vec2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);
+}
+
+vec2 cdiv(vec2 a, vec2 b)
+{
+    return cmul(a, cconj(b)) / nsq(b);
+}
+
+vec2 cexp(vec2 z)
+{
+	return exp(z.x) * vec2(cos(z.y), sin(z.y));
+}
+
+vec2 clog(vec2 z)
+{
+	return vec2(log(cabs(z)), atan(z.y, z.x));
+}
+
+vec2 cpow(vec2 z, vec2 a)
+{
+	return cexp(cmul(a, clog(z)));
+}
+
+
+vec2 ccos(vec2 z)
+{
+	return(cexp(vec2(-z.y, z.x)) + cexp(vec2(z.y, -z.x))) / 2.0;
+}
+
+vec2 csin(vec2 z)
+{
+	vec2 t =(cexp(vec2(-z.y, z.x)) - cexp(vec2(z.y, -z.x))) / 2.0;
+	return vec2(-t.y, -t.x);
+}
+
+vec2 ctan(vec2 z)
+{
+	return cdiv(csin(z), ccos(z));
+}
+
+vec2 ccosh(vec2 z)
+{
+	return(cexp(z) + cexp(-z)) / 2.0;
+}
+
+vec2 csinh(vec2 z)
+{
+	return(cexp(z) - cexp(-z)) / 2.0;
+}
+
+vec2 ctanh(vec2 z)
+{
+	return cdiv(csinh(z), ccosh(z));
+}
+
+vec2 cintpow(vec2 z, int power)
+{
+    bool isneg = power < 0;
+    if (isneg) power = -power;
+    vec2 res = vec2(1, 0);
+    for (int i = 0; i < power; i++)
+    {
+        res = cmul (res, z);
+    }
+    return isneg ? cdiv(vec2(1, 0), res) : res;
+}
+
+int runSeq(vec2 z0, vec2 c)
+{
+    vec2 z = z0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nsq(z) > 4) return i;
+        z = cintpow(z, p) + c;
+    }
+
+    return -1;
+}
+
+int julia(vec2 z0)
+{
+    vec2 c = magnets[0];
+    return runSeq(z0, c);
+}
+
+int mandelbrot(vec2 c)
+{
+    return runSeq(c, c);
+}
+
+// pour passer des coordonnées écran aux coordonnées math
+vec2 screenToMath(vec2 v) { return zoom * (v + offset); }
+vec2 mathToScreen(vec2 v) { v /= zoom; return (v - offset); }
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main()
+{
+    vec2 xy = vec2(gl_FragCoord.x, gl_FragCoord.y);
+    vec2 p0 = screenToMath(xy);
+
+    if (usePoints && useJulia) // coloration des aimants en violet
+    {
+        for (int i = 0; i < N; i++)
+        {
+            if (nsq(xy-mathToScreen(magnets[i])) <= r2)
+            {
+                fragColor = colors[Nmax+1];
+                return;
+            }
+        }
+    }
+
+    if (useLines) // coloration des lignes noires
+    {
+        vec2 zxy = mathToScreen(vec2(0,0));
+        if (abs(xy.x - zxy.x) < 1 || abs(xy.y - zxy.y) < 1)
+        {
+            fragColor = vec4(0,0,0,0);
+            return;
+        }
+    }
+
+    int r = useJulia ? julia(p0) : mandelbrot(p0); // simulation
+    float hue = log(float(r))/log(float(n));
+    float saturation = 1.0;
+    float value;
+    float t;
+    if (r == -1) // choix de la couleur en conséquence
+    {
+        // fragColor = colors[0];
+        value = 0.0;
+    }
+    else
+    {
+        value = 1.0;
+        // t = 1.0 - log(float(r))/log(float(n));
+        // fragColor = t * colors[1];
+    }
+    // fragColor = t * colors[0] + (1-t) * colors[1];
+    fragColor = hsv2rgb(vec3(hue, saturation, value)).xyzz;
+}
